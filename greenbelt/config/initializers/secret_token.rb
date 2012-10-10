e31c11472b0aea05b5840b97fdbb9f8afd8b878e
Helen Lam <hue_lam90@berkeley.edu>
2012-10-09 20:52:06 -0700

    init

---
+++ greenbelt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenbelt::Application.config.secret_token = '1a1d8a19728ec6a9ab3e9d16c420e0e37d75553299c5d7e011daff4bfc8f89f474dfd03f38d7a707fb3cb09fdd51a9cd4f6b724b06c5eb735efb23cddf7023f9'
