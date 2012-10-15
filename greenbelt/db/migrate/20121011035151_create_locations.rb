c4fa83370077c41a6953cc972492e0f87b6660a4
Borislav Petrov <borislav.petrov@gmail.com>
2012-10-14 22:25:26 -0700

    removed bs "locations" migration

--- greenbelt/db/migrate/20121011035151_create_locations.rb
+++
@@ -1,9 +0,0 @@
-class CreateLocations < ActiveRecord::Migration
-  def change
-    create_table :locations do |t|
-      t.name :string
-      t.address :string
-      t.timestamps
-    end
-  end
-end
